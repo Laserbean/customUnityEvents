8652b7f099b670e0119b7cc3bd5410ecac8c1b80
laserbean <laserbean00001@gmail.com>
2023-12-03 13:14:56 +0800

    reformated

--- GameEvent.cs
+++ GameEvent.cs
@@ -5,59 +5,65 @@ using UnityEngine;
 [CreateAssetMenu]
 public class GameEvent : ScriptableObject
 {
-	private List<GameEventListener> listeners = 
-		new List<GameEventListener>();
+    private List<GameEventListener> listeners = new ();
 
-	public void Raise() {
-		for(int i = listeners.Count -1; i >= 0; i--)
-			listeners[i].OnEventRaised();
-	}
+    public void Raise()
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+            listeners[i].OnEventRaised();
+    }
 
-	public void Raise2Int(int num) {
-		for(int i = listeners.Count -1; i >= 0; i--)
-			listeners[i].OnEventRaised(num);
-	}
+    public void Raise2Int(int num)
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+            listeners[i].OnEventRaised(num);
+    }
 
-	public void Raise2Float(float num1) {
-		for(int i = listeners.Count -1; i >= 0; i--)
-			listeners[i].OnEventRaised(num1);
-	}
+    public void Raise2Float(float num1)
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+            listeners[i].OnEventRaised(num1);
+    }
 
-	public void Raise2Bool(bool boo) {
-		for(int i = listeners.Count -1; i >= 0; i--)
-			listeners[i].OnEventRaised(boo);
-	}
+    public void Raise2Bool(bool boo)
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+            listeners[i].OnEventRaised(boo);
+    }
 
-	public void Raise2Vector2(Vector2 vec) {
-		for(int i = listeners.Count -1; i >= 0; i--)
-			listeners[i].OnEventRaised(vec);
-	}
+    public void Raise2Vector2(Vector2 vec)
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+            listeners[i].OnEventRaised(vec);
+    }
 
-	public void Raise2Vector3(Vector3 vec) {
-		for(int i = listeners.Count -1; i >= 0; i--)
-			listeners[i].OnEventRaised(vec);
-	}
+    public void Raise2Vector3(Vector3 vec)
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+            listeners[i].OnEventRaised(vec);
+    }
 
-	public void Raise2Vector2Int(Vector2Int vec) {
-		for(int i = listeners.Count -1; i >= 0; i--)
-			listeners[i].OnEventRaised(vec);
-	}
+    public void Raise2Vector2Int(Vector2Int vec)
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+            listeners[i].OnEventRaised(vec);
+    }
 
-	public void Raise2Vector3Int(Vector3Int vec) {
-		for(int i = listeners.Count -1; i >= 0; i--)
-			listeners[i].OnEventRaised(vec);
-	}
+    public void Raise2Vector3Int(Vector3Int vec)
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+            listeners[i].OnEventRaised(vec);
+    }
 
-	public void Raise2String(string str1) {
-		for(int i = listeners.Count -1; i >= 0; i--)
-			listeners[i].OnEventRaised(str1);
-	}
+    public void Raise2String(string str1)
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+            listeners[i].OnEventRaised(str1);
+    }
 
 
 
-	public void RegisterListener(GameEventListener listener)
-	{ listeners.Add(listener); }
+    public void RegisterListener(GameEventListener listener) => listeners.Add(listener);
 
-	public void UnregisterListener(GameEventListener listener)
-	{ listeners.Remove(listener); }
+    public void UnregisterListener(GameEventListener listener) => listeners.Remove(listener);
 }
