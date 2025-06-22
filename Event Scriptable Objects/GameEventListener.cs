cee5bd411c388b8272ba277cd8fa0fe9caadc1ea
Laserbean <laserbean00001@gmail.com>
2025-06-22 21:35:26 +1200

    Beautiful simple custom editor. Thanks copilot.

--- Event Scriptable Objects/GameEventListener.cs
+++ Event Scriptable Objects/GameEventListener.cs
@@ -12,7 +12,7 @@ namespace Laserbean.CustomUnityEvents
         [Tooltip("Create a GameEvent in the Assets folder. (Create > GameEvent).")]
         public GameEvent Event;
         [Tooltip("Toggles the visibility of event types in the inspector")]
-        public UsedEvents toggleEventVisibility;
+        // public UsedEvents toggleEventVisibility;
 
         [HideInInspector]
         public UnityEvent Response;
@@ -76,20 +76,20 @@ namespace Laserbean.CustomUnityEvents
 
     }
 
-    [System.Flags]
-    public enum UsedEvents
-    {
-        None = 0,
-        Empty = 1,
-        Int = 2,
-        Bool = 4,
-        Float = 8,
-        String = 16,
-        Vector2 = 32,
-        Vector3 = 64,
-        Vector2Int = 128,
-        Vector3Int = 256
-    }
+    // [System.Flags]
+    // public enum UsedEvents
+    // {
+    //     None = 0,
+    //     Empty = 1,
+    //     Int = 2,
+    //     Bool = 4,
+    //     Float = 8,
+    //     String = 16,
+    //     Vector2 = 32,
+    //     Vector3 = 64,
+    //     Vector2Int = 128,
+    //     Vector3Int = 256
+    // }
 
 #if UNITY_EDITOR
     [CustomEditor(typeof(GameEventListener))]
@@ -116,11 +116,13 @@ namespace Laserbean.CustomUnityEvents
 
         public override void OnInspectorGUI()
         {
-            // base.OnInspectorGUI();
-
             this.serializedObject.Update();
 
-            // serializedObject.ApplyModifiedProperties();
+            // Draw the script field like Unity's default inspector
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("m_Script"));
+            EditorGUI.EndDisabledGroup();
+
 
             var myScript = target as GameEventListener;
 
@@ -128,99 +130,48 @@ namespace Laserbean.CustomUnityEvents
             string eventtype = "";
             if (game_event_field.objectReferenceValue)
             {
-                var path = AssetDatabase.GetAssetPath(game_event_field.objectReferenceValue).Split('/');
+                var eventObj = game_event_field.objectReferenceValue;
+                var eventType = eventObj.GetType();
+
+                var path = AssetDatabase.GetAssetPath(eventObj).Split('/');
                 eventname = (path[path.Length - 1]).Split('.')[0];
 
-                if ((myScript.toggleEventVisibility & UsedEvents.Empty) != 0)
-                {
-                    eventtype += " (void)";
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.Int) != 0)
-                {
-                    eventtype += " (int)";
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.Bool) != 0)
-                {
-                    eventtype += " (bool)";
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.String) != 0)
-                {
-                    eventtype += " (string)";
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.Float) != 0)
-                {
-                    eventtype += " (float)";
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.Vector2) != 0)
-                {
-                    eventtype += " (Vector2)";
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.Vector3) != 0)
-                {
-                    eventtype += " (Vector3)";
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.Vector2Int) != 0)
-                {
-                    eventtype += " (Vector2Int)";
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.Vector3Int) != 0)
-                {
-                    eventtype += " (Vector3Int)";
-                }
-                // EditorGUILayout.BeginHorizontal();
-                // try {
-                // EditorGUILayout.LabelField(eventname, SectionNameStyle);
-                // } finally {
-                // EditorGUILayout.EndHorizontal();
-                // }
+                // Optionally, display the type
+                eventtype = $" ({eventType.Name})";
             }
 
-            showInfo = EditorGUILayout.BeginFoldoutHeaderGroup(showInfo, eventname + eventtype);
+            // showInfo = EditorGUILayout.BeginFoldoutHeaderGroup(showInfo, eventname + eventtype);
 
-            if (showInfo)
+            // if (showInfo)
+            // {
+            DrawPropertiesExcluding(serializedObject, _dontIncludeMe);
+
+            if (game_event_field.objectReferenceValue)
             {
-                DrawPropertiesExcluding(serializedObject, _dontIncludeMe);
+                var eventObj = game_event_field.objectReferenceValue;
+                var eventType = eventObj.GetType();
 
-                // myScript.toggleEventVisibility = GUILayout.Toggle(myScript.toggleEventVisibility, "Flag");
-                // myScript.test = GUILayout.Toggle(myScript.test, "Some Bool");
-                if ((myScript.toggleEventVisibility & UsedEvents.Empty) != 0)
-                {
-                    EditorGUILayout.PropertyField(this.serializedObject.FindProperty("Response"), true);
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.Int) != 0)
-                {
+                // Show the UnityEvent field based on the GameEvent type
+                if (eventType.Name == "IntGameEvent")
                     EditorGUILayout.PropertyField(this.serializedObject.FindProperty("IntResponse"), true);
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.Bool) != 0)
-                {
+                else if (eventType.Name == "BoolGameEvent")
                     EditorGUILayout.PropertyField(this.serializedObject.FindProperty("BoolResponse"), true);
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.String) != 0)
-                {
-                    EditorGUILayout.PropertyField(this.serializedObject.FindProperty("StringResponse"), true);
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.Float) != 0)
-                {
+                else if (eventType.Name == "FloatGameEvent")
                     EditorGUILayout.PropertyField(this.serializedObject.FindProperty("FloatResponse"), true);
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.Vector2) != 0)
-                {
+                else if (eventType.Name == "StringGameEvent")
+                    EditorGUILayout.PropertyField(this.serializedObject.FindProperty("StringResponse"), true);
+                else if (eventType.Name == "Vector2GameEvent")
                     EditorGUILayout.PropertyField(this.serializedObject.FindProperty("Vector2Response"), true);
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.Vector3) != 0)
-                {
+                else if (eventType.Name == "Vector3GameEvent")
                     EditorGUILayout.PropertyField(this.serializedObject.FindProperty("Vector3Response"), true);
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.Vector2Int) != 0)
-                {
+                else if (eventType.Name == "Vector2IntGameEvent")
                     EditorGUILayout.PropertyField(this.serializedObject.FindProperty("Vector2IntResponse"), true);
-                }
-                if ((myScript.toggleEventVisibility & UsedEvents.Vector3Int) != 0)
-                {
+                else if (eventType.Name == "Vector3IntGameEvent")
                     EditorGUILayout.PropertyField(this.serializedObject.FindProperty("Vector3IntResponse"), true);
-                }
-
+                else // fallback for base GameEvent or unknown types
+                    EditorGUILayout.PropertyField(this.serializedObject.FindProperty("Response"), true);
             }
+            // }
 
             EditorGUILayout.EndFoldoutHeaderGroup();
             this.serializedObject.ApplyModifiedProperties();
